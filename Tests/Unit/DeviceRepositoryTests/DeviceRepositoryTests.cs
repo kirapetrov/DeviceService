d5828b2c516bbcd40e43ead614d504dc7fecff5c
petrov kirill <petrovki@live.com>
2023-09-04 23:22:49 +0300

    add tests

--- Tests/Unit/DeviceRepositoryTests/DeviceRepositoryTests.cs
+++ Tests/Unit/DeviceRepositoryTests/DeviceRepositoryTests.cs
@@ -1,4 +1,5 @@
 using DeviceRepository.Entities;
+using DeviceRepository.Models;
 using DeviceRepository.Repositories;
 using MockQueryable.Moq;
 using Moq;
@@ -23,6 +24,35 @@ public class DeviceRepositoryTests
         var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
         var actual = await sut.GetAsync(1).ConfigureAwait(false);
         Assert.NotNull(actual);
+    }
+
+    [Fact]
+    public async void UpdateAsync_UpdateNullModel_EqualFlse()
+    {
+        var deviceContext = GetContextMock(Enumerable.Empty<Device>());
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
+        var actual = await sut.UpdateAsync(1, null).ConfigureAwait(false);
+        Assert.Equal(false, actual);
+    }
+
+    [Fact]
+    public async void UpdateAsync_UpdateNotExistingModel_EqualFlse()
+    {
+        var deviceContext = GetContextMock(Enumerable.Empty<Device>());
+        var deviceModel = new DeviceModel();
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
+        var actual = await sut.UpdateAsync(1, deviceModel).ConfigureAwait(false);
+        Assert.Equal(false, actual);
+    }
+
+    [Fact]
+    public async void UpdateAsync_UpdateExistingModel_EqualTrue()
+    {
+        var deviceContext = GetContextMock(new List<Device> { new Device { Id = 1 } });
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
+        var deviceModel = new DeviceModel();
+        var actual = await sut.UpdateAsync(1, deviceModel).ConfigureAwait(false);
+        Assert.Equal(true, actual);
     }
 
     [Fact]
