993a094ccffde10ee83f8f7384a58b31b91f6002
petrov kirill <petrovki@live.com>
2023-09-14 22:19:38 +0300

    add more tests

--- Tests/Unit/DeviceRepositoryTests/DeviceRepositoryTests.cs
+++ Tests/Unit/DeviceRepositoryTests/DeviceRepositoryTests.cs
@@ -8,11 +8,13 @@ namespace DeviceRepositoryTests;
 
 public class DeviceRepositoryTests
 {
+    private readonly Device deviceStub = new Device { Id = 1 };
+
     [Fact]
     public async void GetAsync_GetNotExistsModel_IsNull()
     {
-        var deviceContext = GetContextMock(Enumerable.Empty<Device>());
-        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
+        var deviceContextMock = GetContextMock();
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContextMock.Object);
         var actual = await sut.GetAsync(1).ConfigureAwait(false);
         Assert.Null(actual);
     }
@@ -20,17 +22,35 @@ public class DeviceRepositoryTests
     [Fact]
     public async void GetAsync_GetExistingModel_IsNotNull()
     {
-        var deviceContext = GetContextMock(new List<Device> { new Device { Id = 1 } });
-        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
+        var deviceContextMock = GetContextMock(deviceStub);
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContextMock.Object);
         var actual = await sut.GetAsync(1).ConfigureAwait(false);
         Assert.NotNull(actual);
     }
 
     [Fact]
+    public async void AddAsync_AddNullModel_EqualFlse()
+    {
+        var deviceContextMock = GetContextMock();
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContextMock.Object);
+        var actual = await sut.AddAsync(null).ConfigureAwait(false);
+        Assert.Equal(-1, actual);
+    }
+
+    [Fact (Skip = "How to mock EntityEntry")]
+    public async void AddAsync_AddNotNullModel_EqualTrue()
+    {
+        var deviceContextMock = GetContextMock();
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContextMock.Object);
+        var actual = await sut.AddAsync(new DeviceModel()).ConfigureAwait(false);
+        Assert.Equal(1, actual);
+    }
+
+    [Fact]
     public async void UpdateAsync_UpdateNullModel_EqualFlse()
     {
-        var deviceContext = GetContextMock(Enumerable.Empty<Device>());
-        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
+        var deviceContextMock = GetContextMock();
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContextMock.Object);
         var actual = await sut.UpdateAsync(1, null).ConfigureAwait(false);
         Assert.False(actual);
     }
@@ -38,9 +58,9 @@ public class DeviceRepositoryTests
     [Fact]
     public async void UpdateAsync_UpdateNotExistingModel_EqualFlse()
     {
-        var deviceContext = GetContextMock(Enumerable.Empty<Device>());
+        var deviceContextMock = GetContextMock();
         var deviceModel = new DeviceModel();
-        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContextMock.Object);
         var actual = await sut.UpdateAsync(1, deviceModel).ConfigureAwait(false);
         Assert.False(actual);
     }
@@ -48,8 +68,8 @@ public class DeviceRepositoryTests
     [Fact]
     public async void UpdateAsync_UpdateExistingModel_EqualTrue()
     {
-        var deviceContext = GetContextMock(new List<Device> { new Device { Id = 1 } });
-        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
+        var deviceContextMock = GetContextMock(deviceStub);
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContextMock.Object);
         var deviceModel = new DeviceModel();
         var actual = await sut.UpdateAsync(1, deviceModel).ConfigureAwait(false);
         Assert.True(actual);
@@ -58,8 +78,8 @@ public class DeviceRepositoryTests
     [Fact]
     public async void DeleteAsync_DeleteNotExistingModel_EqualFlse()
     {
-        var deviceContext = GetContextMock(Enumerable.Empty<Device>());
-        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
+        var deviceContextMock = GetContextMock();
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContextMock.Object);
         var actual = await sut.DeleteAsync(1).ConfigureAwait(false);
         Assert.False(actual);
     }
@@ -67,14 +87,19 @@ public class DeviceRepositoryTests
     [Fact]
     public async void DeleteAsync_DeleteExistingModel_EqualTrue()
     {
-        var deviceContext = GetContextMock(new List<Device> { new Device { Id = 1 } });
-        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContext);
+        var deviceContextMock = GetContextMock(deviceStub);
+        var sut = new DeviceRepository.Repositories.DeviceRepository(deviceContextMock.Object);
         var actual = await sut.DeleteAsync(1).ConfigureAwait(false);
-        Assert.True(true);
+
+        Assert.True(actual);
     }
 
-    private DeviceContext GetContextMock(IEnumerable<Device> devices)
+    private Mock<DeviceContext> GetContextMock(Device? device = null)
     {
+        var devices = device == null ?
+            Enumerable.Empty<Device>() :
+            new List<Device> { device };
+
         var deviceContextMock = new Mock<DeviceContext>();
         deviceContextMock
             .SetupGet(x => x.Devices)
@@ -83,9 +108,22 @@ public class DeviceRepositoryTests
         deviceContextMock
             .Setup(x => x.SaveChangesAsync(CancellationToken.None));
 
-        deviceContextMock
-            .Setup(x => x.Remove(It.IsAny<object>()));
+        if (device != null)
+        {
+            //TODO mock EntityEntry
+            // var entryMock = new Mock<InternalEntityEntry>();
+            // entryMock
+            //     .SetupGet(x => x.Entity)
+            //     .Returns(device);
 
-        return deviceContextMock.Object;
+            // deviceContextMock
+            //     .Setup(x => x.Add(It.IsAny<object>()))
+            //     .Returns(new EntityEntry(entryMock.Object));
+        }
+
+        deviceContextMock
+                .Setup(x => x.Remove(It.IsAny<object>()));
+
+        return deviceContextMock;
     }
 }
